6ab32f274b3601beb5f0ae2150dca97642b372de
Ahmad <ahmadhussain03@hotmail.com>
2022-04-29 23:53:05 +0500

    lat lng updated

--- src/components/main/Map.jsx
+++ src/components/main/Map.jsx
@@ -1,5 +1,8 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import React, {useState, useEffect, useRef} from 'react'
 import mapboxgl from '!mapbox-gl'; // eslint-disable-line import/no-webpack-loader-syntax
+import Select from '../../components/main/form/Select'
+import axios, { apiUrl } from '../../utils/axios'
 
 mapboxgl.accessToken = 'pk.eyJ1IjoiYWhtYWRodXNzYWluIiwiYSI6ImNrZTVlemdoMDEyOGwycm1zdjZpcmNjYTcifQ.c6fY2tGCyssRuoyGCIuAJw';
 
@@ -7,6 +10,7 @@ const Map = ({ devices }) => {
 
     const mapContainer = useRef(null);
     const map = useRef(null);
+    const [device, setDevice] = useState(null);
     const [lng, setLng] = useState(-70.9);
     const [lat, setLat] = useState(42.35);
     const [zoom, setZoom] = useState(8);
@@ -19,27 +23,83 @@ const Map = ({ devices }) => {
             center: [lng, lat],
             zoom: zoom
         });
-    });
+    }, []);
+
+    const handleDeviceChange = (value) => {
+        setDevice(value)
+    }
+
+    const stringToChanks = (string, chunkSize) => {
+        const chunks = [];
+        while (string.length > 0) {
+            chunks.push(string.substring(0, chunkSize));
+            string = string.substring(chunkSize, string.length);
+        }
+        return chunks
+    }
+
+    const HexToFloat32 = (str) => {
+        var int = parseInt(str, 16);
+        if (int > 0 || int < 0) {
+            var sign = (int >>> 31) ? -1 : 1;
+            var exp = (int >>> 23 & 0xff) - 127;
+            var mantissa = ((int & 0x7fffff) + 0x800000).toString(2);
+            var float32 = 0
+            for (var i = 0; i < mantissa.length; i += 1) { float32 += parseInt(mantissa[i]) ? Math.pow(2, exp) : 0; exp-- }
+            return (float32 * sign).toFixed(4);
+        } else return 0
+    }    
 
     useEffect(() => {
-        for(let device of devices){
-            new mapboxgl.Marker().setLngLat([device.location.lng, device.location.lat]).setPopup(
-                new mapboxgl.Popup({ offset: 25 })
-                  .setHTML(
-                    `<h2>${device.name}</h2>`
-                  )
-              ).addTo(map.current);
+        if(device !== null) {
+            axios.get(`device/${device.value}`).then(response => {
+                const mappedLatLng = response.data.data.map(device => {
+
+                    const returnedObject = {};
+
+                    const chunks = stringToChanks(device.data, 8)
+                    const firstPart = chunks[0];
+                    const secondPart = chunks[1];
+                    const convertedString = HexToFloat32(firstPart);
+                    const convertedStringTwo = HexToFloat32(secondPart);
+
+                    const splittedString = convertedString.split('.');
+                    const splittedStringTwo = convertedStringTwo.split('.');
+                    
+                    if(splittedString[0].length < 2) {
+                        const secondNumber = "." + splittedString[1];
+                        returnedObject.latitude = (splittedString[0] * -1) + (parseFloat(secondNumber) / 60)
+                    } else {
+                        const firstTwoDigits = splittedString[0].slice(0, 2);
+                        const remainingDigits = splittedString[0].slice(2, splittedString[0].length) + '.' + (splittedString[1]);
+
+                        returnedObject.latitude = (firstTwoDigits * -1) + (remainingDigits / 60)
+                    }
+
+                    if(splittedStringTwo[0].length < 2) {
+                        const secondNumber = "." + splittedStringTwo[1];
+                        console.log(parseFloat(secondNumber) / 60, splittedStringTwo[0])
+                        returnedObject.longitude = (splittedStringTwo[0]) + (parseFloat(secondNumber) / 60)
+                    } else {
+                        const firstTwoDigits = splittedStringTwo[0].slice(0, 2);
+                        const remainingDigits = splittedStringTwo[0].slice(2, splittedStringTwo[0].length) + '.' + (splittedStringTwo[1]);
+
+                        returnedObject.longitude = (firstTwoDigits) + (remainingDigits / 60)
+                    }
+
+                    return returnedObject;
+                })
+
+                console.log(mappedLatLng)
+            }).catch(error => console.error(error))
         }
-        if(devices.length){
-            map.current.flyTo({
-                center: [devices[0].location.lng, devices[0].location.lat],
-                zoom: 3
-            })
-        }
-    }, [devices])
+    }, [device]);
 
     return (
         <div>
+            <div className="py-2">
+                <Select value={device} onChange={handleDeviceChange} placeholder="Devices" options={devices.map(d => ({ value: d.id, text: d.id }))}></Select>
+            </div>
             <div ref={mapContainer} className="rounded-lg" style={{height: '300px', width: '100%'}} />
         </div>
     )
--- src/screens/animal/CreateAnimal.jsx
+++ src/screens/animal/CreateAnimal.jsx
@@ -26,37 +26,6 @@ const diseaseOptions = [
     {value: 'healthy', text: 'Healthy'},
 ];
 
-const typeOptions = [
-    {value: 'pig', text: 'Pig'},
-    {value: 'cattle', text: 'Cattle'},
-    {value: 'goat', text: 'Goat'},
-    {value: 'sheep', text: 'Sheep'},
-];
-
-const breedOptions = {
-    pig: [
-        "Pig Breed","Red Wattle","Mukota","Mora Romagnola","Poland China", "Large White", "Ossabaw Island", "Spotted", "Lacombe",
-            "Thuoc Nhieu", "Norwegian Landrace","French Landrace","Swallow Belied Mangalitza","Cantonese Pig","Mulefoot","Krskopolje", "Minzhu", "Mangalitza",
-            "Gloucestershire Old Spots","British Landrace","Bantu","Philippine Native","Chester White","Welsh","Wuzhishan","Lithuanian Native","Saddleback",
-            "German Landrace","Tamworth","Belgian Landrace","Iberian","Hereford","Beijing Black","Large Black","Fengjing","Guinea Hog","Mong Cai","Meishan",
-            "Jinhua","American Landrace","Hezuo","Duroc","Danish Landrace","Turopolje","Neijiang","Oxford Sandy and Black","Swedish Landrace","Bulgarian White",
-            "Middle White", "Italian Landrace","Belarus Black Pied","Czech Improved White","American Yorkshire","Large Black-white","Ba Xuyen","Choctaw Hog",
-            "Hampshire","British Lop" ,"Vietnamese Potbelly","Berkshire","Kunekune","Pietrain","Dutch Landrace","Ningxiang","Kele","Tibetan","Arapawa Island",
-            "Yorkshire", "Finnish Landrace","Angeln saddleback", "Other"
-    ],
-    goat: [
-        "Goat Breed","Angora", "Boer","LaMancha","Nubian" ,"Oberhasli" ,"Saanen","Toggenburg","Other"
-    ],
-    cattle: [
-        "Cattle Breed","Afrikaner Cattle", "Angus Beef/Aberdeen Angus", "Ankole Cattle", "Beefmaster Cattle","Bonsmara Cattle",
-            "Boran Cattle","Brahman Cattle", "Braunvieh Cattle", "Charolais Cattle", "Drakensberger Cattle","Hereford Cattle","Limousin Cattle",
-            "Nguni Cattle","Santa Gertrudis Cattle","Shorthorn Cattle","Simbra Cattle","Simmentaler Cattle", "Sussex Cattle","Tuli Cattle","Wagyu Beef","Other"
-    ],
-    sheep: [
-        "Other"
-    ]
-};
-
 const CreateAnimal = ({ user }) => {
 
     const [animalId, setAnimalId] = useState("")
